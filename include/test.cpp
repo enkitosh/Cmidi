16ba5109efb11e2f124a183a5659442f0964a554
Thordur Hermannsson <thordurguhe@gmail.com>
2012-10-27 16:56:50 +0000

    update

--- include/test.cpp
+++ include/test.cpp
@@ -9,16 +9,25 @@ int main( int argc, char *argv[] )
     int LOOP = 0; 
     string str = argv[1];
     string ttl = argv[2];
-    BPM = convertToInt(argv[4]); 
 
-     if(convertToInt(argv[6]) < width-1)
+//is BPM specified??
+	if(argc > 3)
+	{
+    		BPM = convertToInt(argv[4]); 
+	}
+	else
+	{
+		BPM = 120;
+	}
+//is loop specified??
+     if((argc > 5) && (convertToInt(argv[6]) < width-1))
      { 
 	LOOP = convertToInt(argv[6]); 
      }
     else
     {
 	//loop can never be bigger than this
-	LOOP = width-1;
+	LOOP = width-2;
     }
     //***COLOR CHECKING***//
     int value = convertToInt(&str[str.length()-1]);
